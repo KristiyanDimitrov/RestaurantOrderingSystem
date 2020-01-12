7fa2bbcfb4bc689a69d63b945f371f9e059a08ec
Kristiyan Dimitrov <Kristiyan_D@outlook.com>
2020-01-12 12:02:47 +0000

    Add files via upload
    Base

---
+++ RetorantOrderingSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantOrderingSystem
+{
+    class Interface
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> Tables = new Dictionary<int, int>()
+            { {1, 4},{1, 4},{1, 4},{1, 4},{1, 6},{1, 6},{1, 6},{1, 8},{1, 3},{1, 2} }; // Table Numbers and their capacity
+
+
+            Dictionary<string, int> Waiters = new Dictionary<string, int>()
+            { {"Daisy", 3},{"Monica", 3},{"Alex", 2},{"Lisa", 2},{"Daren",3 },{"David",2} }; // Waiters and the max numnber of tables they can cater to
+
+           
+
+
+
+
+
+        }
+    }
+}
